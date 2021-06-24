1f2b7f68c26bf740c7f5c2b6d88a3979db87bd71
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2021-06-23 21:58:38 -0300

    Aula 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import { database } from '../services/firebase';
+
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
-const Home: React.FC = () => {
+export const Home: React.FC = () => {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -19,6 +23,25 @@ const Home: React.FC = () => {
     }
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    console.log(`rooms/${roomCode}`);
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -39,8 +62,13 @@ const Home: React.FC = () => {
             Crie sua sala com o Google
           </button>
           <div className='separator'>ou entre em uma sala</div>
-          <form>
-            <input type='text' placeholder='Digite o código da sala' />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type='text'
+              placeholder='Digite o código da sala'
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type='submit'>Entrar na sala</Button>
           </form>
         </div>
@@ -48,5 +76,3 @@ const Home: React.FC = () => {
     </div>
   );
 };
-
-export default Home;
